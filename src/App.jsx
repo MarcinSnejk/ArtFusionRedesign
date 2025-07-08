841e6a525097e7f8f76280f17b8e220d45587e89
Marcin <marcinw856@gmail.com>
2025-07-08 10:15:18 +0200

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import ForArtist from "./components/ForArtist";
 import Herosection from "./components/Herosection";
 import HowWorks from "./components/HowWorks";
-import Offer from "./components/offer";
+import Offer from "./components/Offer";
 import Social from "./components/Social";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
